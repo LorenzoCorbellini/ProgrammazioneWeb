930ae08e55c2e9727e3a2017529b943144c80cb2
LorenzoCorbellini <l.corbellini@studenti.unibg.it>
2026-05-10 15:21:41 +0200

    add .env file for db credentials

--- src/config/db.php
+++ src/config/db.php
@@ -1,8 +1,10 @@
 <?php
-$host     = '127.0.0.1';
-$dbname   = 'my_plainasia';
-$username = 'root';
-$password = 'root'; 
+$env = parse_ini_file(__DIR__ . '/../.env');
+
+$host = $env['DB_HOST'];
+$dbname = $env['DB_NAME'];
+$username = $env['DB_USER'];
+$password = $env['DB_PASS'];
 
 try {
     $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password);
