5ee51f4acf7f0a6a238ba49d98ca3c3709808e34
Stefan Dolete <m.dolete@studenti.unibg.it>
2026-05-16 19:30:49 +0200

    utente e gruppi (base)

--- src/gruppi.php
+++ src/gruppi.php
@@ -1,3 +1,7 @@
+<?php
+require_once __DIR__ . '/config/db.php';
+require_once __DIR__ . '/functions.php';
+?>
 <!DOCTYPE html>
 <html>
 
@@ -13,9 +17,46 @@
 	<?php
 	include 'nav.html';
 	include 'footer.html';
-	?>
+	
 	<div id="content">
+		
+			$sql = "
+			SELECT 
+				utente.nickname,
+				gruppo.nome,
+				gruppo.dataCreazione
+			FROM gruppo
+			JOIN utente
+			ON gruppo.creatoDa = utente.codice
+			";
 
+			$stmt = $pdo->query($sql);
+			$stmt->setFetchMode(PDO::FETCH_ASSOC);
+
+			echo "<table border='1'>";
+			echo "
+			<tr>
+		
+				<th>Creato da</th>
+				<th>Nome gruppo</th>
+				<th>Data creazione</th>
+			</tr>
+			";
+
+			foreach($stmt as $row){
+
+				echo "<tr>";
+
+				echo "<td>".$row['codice']."</td>";
+				echo "<td>".$row['nickname']."</td>";
+				echo "<td>".$row['nome']."</td>";
+				echo "<td>".$row['dataCreazione']."</td>";
+
+				echo "</tr>";
+			}
+
+			echo "</table>";
+		?>
 	</div>
 
 </body>
--- src/utenti.php
+++ src/utenti.php
@@ -1,3 +1,7 @@
+<?php
+require_once __DIR__ . '/config/db.php';
+require_once __DIR__ . '/functions.php';
+?>
 <!DOCTYPE html>
 <html>
 <head>
@@ -12,7 +16,28 @@
 	include 'footer.html';
 ?>
 	<div id="content">     
+		<?php
+			$stmt = $pdo->query("SELECT * FROM utente");
+	echo "<table border='1'>
+			<tr>
+				<th>Nickname</th>
+				<th>Nome</th>
+				<th>Cognome</th>
+				<th>Data nascita</th>
+			</tr>";
+				
+		foreach($stmt as $row){
 
+		echo "<tr>";
+			echo "<td>".$row['nickname']."</td>";
+			echo "<td>".$row['nome']."</td>";
+			echo "<td>".$row['cognome']."</td>";
+			echo "<td>".$row['dataNascita']."</td>";
+		echo "</tr>";
+		}
+
+	echo "</table>";
+		?>
 	</div>
 
 </body>
